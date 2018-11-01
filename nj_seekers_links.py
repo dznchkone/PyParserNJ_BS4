8e787ba7f44c5e9a07ddaddd0be85b45bf921015
Igor Vavilin <igorvv.vn@gmail.com>
2018-11-01 14:37:30 +0300

    Add Regex write to csv

--- nj_seekers_links.py
+++ nj_seekers_links.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+import csv
 
 MAIN_URL = 'http://www.novjob.ru/'
 
 number_of_pages = 0
 
 CRAWLED_RLINKS = []
+
+
 
 
 def get_page_content(url):
@@ -15,7 +19,7 @@ def get_page_content(url):
 
 
 def get_page_links(content):
-    soup = BeautifulSoup(content, 'lxml')
+    soup = BeautifulSoup(content, 'html.parser')
     links_short = soup.select('.hdr')
     links = []
     for short_link in links_short:
@@ -29,15 +33,25 @@ def fill_crawled_links(links_on_cpage):
         print(f'Crawled: {link}')
 
 
-def get_data(content):
+def get_data(page_link,content):
+    parse_data ={'link': page_link}
     soup = BeautifulSoup(content, 'lxml')
     tds = soup.find('div', class_='osn').find_all('td')
     for i, td in enumerate(tds):
+        td_text = td.text
+        parse_data[i] = re.sub("^\s+|\n|\r|\s+$", '', td_text)
+        # TODO add Regex for date \d{2}[.]\d{2}[.]\d{4}
         print('------------------------------------------------------------------------------')
-        print(f'|||{i}||| TD is: {td}')
+        print(f'|||{i}||| TD is: {td_text}')
         print('------------------------------------------------------------------------------')
 
-    return tds
+    return parse_data
+
+def write_csv(data):
+    with open('nj_resumes_list.txt', 'a') as f:
+        fieldnames = ['link', 0,1,2,3,4,5,6,7,8,9]
+        writer = csv.DictWriter(f,fieldnames=fieldnames, delimiter="|")
+        writer.writerow(data)
 
 
 def main():
@@ -52,9 +66,8 @@ def main():
         fill_crawled_links(links_on_cpage)
     for i, link in enumerate(CRAWLED_RLINKS):
         page_content = get_page_content(link)
-        data = get_data(page_content)
-
-
+        data = get_data(link, page_content)
+        write_csv(data)
 
     input('Press Enter to exit...')
 
